7b7f8ad5e1c5fc82659b08928608e75aa9d9f634
maxsynnott <maxryansynnott@gmail.com>
2020-03-12 16:09:38 +0700

    generate random 500 x 500 grid

--- client/src/components/Grid.vue
+++ client/src/components/Grid.vue
@@ -1,8 +1,9 @@
 <template>
 	<div id="grid">
-		<h1>Grid</h1>
+		<!-- <h1>{{ title }}</h1> -->
 
 		<canvas 
+			id="canvas"
 			height="500px"
 			width="500px">
 		</canvas>
@@ -14,3 +15,29 @@
 		border: 1px solid black;
 	}
 </style>
+
+<script>
+	
+
+	export default {
+		name: 'grid',
+		data () {
+
+		},
+		mounted() {
+			const canvas = document.getElementById('canvas');
+			const ctx = canvas.getContext('2d');
+
+			let pixelData = [];
+
+			for (let i = 0; i < 500 * 500 * 4; i++) {
+				pixelData.push(Math.round(Math.random() * 255))
+			}
+
+			const clampedPixelArray = Uint8ClampedArray.from(pixelData);
+			const pixelGridData = new ImageData(clampedPixelArray, 500, 500);
+
+			ctx.putImageData(pixelGridData, 0, 0);
+		}
+	}
+</script>
