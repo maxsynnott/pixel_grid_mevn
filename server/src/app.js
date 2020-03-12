e94aa6c3f3f514fdd015998dc14bef2d5ecce0f0
maxsynnott <maxryansynnott@gmail.com>
2020-03-12 14:56:29 +0700

    node init

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const express = require('express')
+
+const app = express()
+
+app.get('/test', (req, res) => {
+  res.send({
+      message: "Hello World!"	
+  })
+})
+
+app.listen(process.env.PORT || 8081)
