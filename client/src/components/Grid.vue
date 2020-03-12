4848de178bd972cfe092fa3538c995916da366bd
maxsynnott <maxryansynnott@gmail.com>
2020-03-12 15:35:32 +0700

    added canvas

--- client/src/components/Grid.vue
+++ client/src/components/Grid.vue
@@ -1,5 +1,16 @@
 <template>
 	<div id="grid">
 		<h1>Grid</h1>
+
+		<canvas 
+			height="500px"
+			width="500px">
+		</canvas>
 	</div>
 </template>
+
+<style>
+	canvas {
+		border: 1px solid black;
+	}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Grid from '../components/Grid.vue'
+import Grid from '@/components/Grid.vue'
 
 Vue.use(VueRouter)
 
