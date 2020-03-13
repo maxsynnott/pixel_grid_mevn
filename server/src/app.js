58224e53237311003970795ff223ff3565f5a0ff
maxsynnott <maxryansynnott@gmail.com>
2020-03-13 14:55:55 +0700

    create api in and working

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,74 @@
+// Mongoose setup
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/grids');
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error"));
+
+db.once("open", function(callback) {
+	console.log("Connection Succeeded");
+});
+//
+
+// Model requires
+	const Grid = require('../models/grid')
+// 
+
+// Express setup
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json())
+//
 
-app.get('/test', (req, res) => {
-  res.send({
-    message: "Hello World!"	
-  })
+app.post('/grids', (req, res) => {
+	const db = req.db;
+
+	console.log('below')
+	console.log(req)
+	console.log(req.body)
+
+	const width = req.body.width;
+	const height = req.body.height;
+	const pixels = [];
+
+	for (let i = 0; i < width * height; i++) {
+		for (let j = 0; j < 3; j++) {
+			pixels.push(Math.round(Math.random() * 255))
+		}
+
+		pixels.push(255)
+	}
+
+	const new_grid = new Grid({
+		width: width,
+		height: height,
+		pixels: pixels
+	})
+
+	new_grid.save(function (error) {
+		if (error) {
+			res.send({
+				success: false
+			})
+		} else {
+			res.send({
+				success: true
+			})
+		}
+	})
 })
+
+
+
+
+
 
 app.get('/random/:width/:height', (req, res) => {
 	pixelData = []
